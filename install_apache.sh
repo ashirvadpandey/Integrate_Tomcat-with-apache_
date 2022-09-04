39897c9bd439230f7e94c1d3460dfc7f4246e379
Ashirvad Pandey <72142699+ashirvadpandey@users.noreply.github.com>
2022-09-05 00:21:08 +0530

    Create install_apache.sh

---
+++ install_apache.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+yum update -y
+yum install -y httpd.x86_64
+systemctl start httpd.service 
+systemctl enable httpd.service
+echo “Hello CloudChamp from $(hostname -f).Created by USERDATA in Terraform. SUBSCRIBE NOW!!” (arrow_symbol) /var/www/html/index.html
